21d3fb6242c23bbecfac91b5698f389348acaebd
Eduardo <edwcrz@gmail.com>
2026-04-28 22:45:38 -0300

    Primer commit Clase02 Ej01 y Ej02

---
+++ Clase02Ej02.py
@@ -0,0 +1,14 @@
+try:
+    numero = int(input("Ingresá un número: "))
+    print("Número válido:", numero)
+    if numero > 5:
+        print("El número es mayor que 5")
+    elif numero == 5:
+        print("El número es igual a 5")
+    else:
+        print("El número es menor que 5")
+except ValueError:
+    print("Error: no ingresaste un entero")
+usuario = str(input("Ingrese el nombre: "))
+print ("el usuario ", usuario, "ingreso el numero: ", numero )
+usuario.upper
