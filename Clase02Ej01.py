7eec5091310aa4cd3f243b32779328f7a634c644
Eduardo <edwcrz@gmail.com>
2026-05-02 16:47:27 -0300

    primer cambio luego de subir a github repo Tecno3FPythonIntro

--- Clase02Ej01.py
+++ Clase02Ej01.py
@@ -3,5 +3,7 @@ a = 10
 a = int(input("Enter a number: "))
 if (a > 5):
      print("a is greater than 5")
+elif (a == 5):
+    print("a is equal to 5")
 else:
-    print("a is not greater than 5")    
+    print("a is not greater than 5")
