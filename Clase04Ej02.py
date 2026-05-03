d634ff9bcf82449d81826baa0e78ed006bc190bc
Eduardo <edwcrz@gmail.com>
2026-05-03 18:56:47 -0300

    01 release del ejercicio 02 pide un nro y le suma 5

---
+++ Clase04Ej02.py
@@ -0,0 +1,4 @@
+## Realizar un programa que le pida al usuario un numero y le sume 5, el resultado debe mostrarce por pantalla
+numero = int(input("Ingrese un numero entero: "))
+resultado = numero + 5
+print("El resultado de la suma es: {resultado}".format(resultado=resultado))
