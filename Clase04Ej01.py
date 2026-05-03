c8f859afa1e934488fa04ce6d8a9c3504d59df96
Eduardo <edwcrz@gmail.com>
2026-05-03 18:48:46 -0300

    01 release del ejercicio 01 de la unidad 04 paso el nombre a mayusculas con upper()

---
+++ Clase04Ej01.py
@@ -0,0 +1,4 @@
+## Realizar un programa que le pida al usuario su nombre y lo muestre en mayúsculas
+nombre = str(input("Ingrese su nombre: "))
+nombremayusculas = nombre.upper()
+print("Su nombre en mayúsculas es: {nombremayusculas}".format(nombremayusculas=nombremayusculas))
