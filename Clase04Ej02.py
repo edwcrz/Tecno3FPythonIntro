34e0c315659aef63e01a18fe0a9141ffb9ca859d
Eduardo <edwcrz@gmail.com>
2026-05-03 19:04:24 -0300

    02 release del ejercicio 02 unidad 04 pide un nro y le suma 5 con control de ingreso de entero

--- Clase04Ej02.py
+++ Clase04Ej02.py
@@ -1,4 +1,8 @@
 ## Realizar un programa que le pida al usuario un numero y le sume 5, el resultado debe mostrarce por pantalla
-numero = int(input("Ingrese un numero entero: "))
-resultado = numero + 5
-print("El resultado de la suma es: {resultado}".format(resultado=resultado))
+try:
+    numero = input("Ingrese un numero entero: ")
+    numeroentero = int(numero)
+    resultado = numeroentero + 5
+    print("El resultado de la suma es: {resultado}".format(resultado=resultado))
+except ValueError:
+    print("Error: El valor ingresado no es un numero entero.")
